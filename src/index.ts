fdfdc81b088cf16d8d7e686ff8568046740fd469
Abhin Dev <63386717+abhindev@users.noreply.github.com>
2023-11-13 11:00:21 +0530

    bao

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import { Elysia } from "elysia";
+import Bao from "baojs";
 
-const app = new Elysia()
-  .get("/", () => "Hello Elysia")
-  .listen(3000, "0.0.0.0");
+const app = new Bao();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.get("/", (ctx) => {
+  return ctx.sendText("Hello World!");
+});
+
+const server = app.listen();
+
+console.log(`Listening on ${server.hostname}:${server.port}`);
+
+// import { Elysia } from "elysia";
+
+// const app = new Elysia()
+//   .get("/", () => "Hello Elysia")
+//   .listen(3000, "0.0.0.0");
+
+// console.log(
+//   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+// );

0ea11554d59e130ff6aed5a31e677a4f9ddcdcf9
Abhin Dev <63386717+abhindev@users.noreply.github.com>
2023-11-13 11:07:08 +0530

    Elysia new test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import Bao from "baojs";
+// import Bao from "baojs";
+import { Elysia } from "elysia";
 
-const app = new Bao();
+// const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+// const app = new Bao();
+const app = new Elysia();
 const port = parseInt(process.env.PORT || "8080");
 
-app.get("/", (ctx) => {
-  return ctx.sendText("Hello world from Bao.js running on Railway!");
-});
+app.get("/", () => "Hello Elysia");
+// app.get("/", () => {
+//   return ctx.sendText("Hello world from Bao.js running on Railway!");
+// });
 
 const server = app.listen({ port: port });
 console.log(`Server listening on ${server.hostname}:${port}`);

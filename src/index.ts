ddcbc508e6e083adf97ab4556c55fe42126eb418
Abhin Dev <63386717+abhindev@users.noreply.github.com>
2023-11-13 10:50:44 +0530

    new Elysia

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-
-const server = app.listen({ port: port });
-console.log(`Server listening on ${server.hostname}:${port}`);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
